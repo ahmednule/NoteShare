411bb951c7c146273afc5a406b2ce40e2735a449
Onyoniken <onyonikenn@gmail.com>
2024-05-21 16:36:53 +0300

    API for search, authentication&permissions

---
+++ elevate/api/urls.py
@@ -0,0 +1,8 @@
+# api/urls.py
+
+from django.urls import path
+from .views import ResourceListCreateView
+
+urlpatterns = [
+    path('resources/', ResourceListCreateView.as_view(), name='resource-list-create'),
+]
--- elevate/elevate/urls.py
+++ elevate/elevate/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('crm.urls')),
+
+    path('api/', include('api.urls')),
 ]
