c1275fc0f97725f906bcf317b3407dfe749b66c9
Nyams <129679320+Kellyjunior6387@users.noreply.github.com>
2024-05-22 15:51:08 +0300

    Delete django/e_learner/noteshare directory
    It was repetitive

--- django/e_learner/noteshare/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Resource
-
-class ResourceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Resource
-        fields = ['id', 'title', 'description', 'file', 'user', 'created_at']
-        read_only_fields = ['user', 'created_at']
--- django/e_learner/noteshare/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('resources/', views.ResourceListCreate.as_view(), name='resource-list-create'),
-    path('resources/<int:pk>/', views.ResourceDetail.as_view(), name='resource-detail'),
-]
