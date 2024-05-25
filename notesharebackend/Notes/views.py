98593e6de98ba4ee17bcbac332a6e894e24945c5
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-05-25 13:43:14 +0300

    Implemented the upload feature and started on getting the user profile

--- notesharebackend/Notes/views.py
+++ notesharebackend/Notes/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework import serializers, views, status
+from rest_framework.parsers import MultiPartParser
+from google.cloud import storage
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from google.auth import default
 
-# Create your views here.
+credentials, project_id = default()
+
+class FileUploadSerializer(serializers.Serializer):
+    file = serializers.FileField()
+
+class FileUploadView(views.APIView):
+    parser_classes = [MultiPartParser]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = FileUploadSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Get the uploaded file
+        file = serializer.validated_data['file']
+
+        # Create a Cloud Storage client
+        client = storage.Client()
+
+        # Get the bucket
+        bucket = client.bucket('nyams-noteshare')
+
+        # Create a new blob and upload the file
+        blob = bucket.blob(file.name)
+        blob.upload_from_file(file)
+
+        # Return a success response
+        return Response({'message': 'File uploaded successfully.'}, status=status.HTTP_201_CREATED)
+
