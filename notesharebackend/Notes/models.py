d1dd074be26ca468e17eb0236b3db2baa75d283a
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-05-27 19:17:35 +0300

    Revert "Merge pull request #7 from maitha1-nule/KO"
    This reverts commit 127d45a505a02a037729d9db4f5460b3d32a9834, reversing
    changes made to 4eaffeb7c501390d75de7d282cfb6644bf2701b8.

--- notesharebackend/Notes/models.py
+++ notesharebackend/Notes/models.py
@@ -1,21 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Resource(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    file = models.FileField(upload_to='resources/')
-    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
-class UserUpload(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='uploads/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.user.username} upload"
-
+# Create your models here.
