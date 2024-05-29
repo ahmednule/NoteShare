403739ac2d3b68f346546cb4470a8749bcdf9a91
Onyoniken <onyonikenn@gmail.com>
2024-05-29 22:33:18 +0300

    changes

--- notesharebackend/User/views.py
+++ notesharebackend/User/views.py
@@ -4,8 +4,13 @@ from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
     #queryset = User.objects.all()
     #serializer_class = UserSerializer
     def get(self, request, id=None):
@@ -17,6 +22,17 @@ class UserViewSet(viewsets.ModelViewSet):
                     return JsonResponse(serializer.data, status=status.HTTP_200_OK)
             except User.DoesNotExist:
                 return JsonResponse({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
-
+            
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })           
 
 
