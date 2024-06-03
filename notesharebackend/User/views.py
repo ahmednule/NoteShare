10315822fbe1f0af9e5af07336ce61153a24471c
Onyoniken <onyonikenn@gmail.com>
2024-06-03 17:41:50 +0300

    user registration endpoint

--- notesharebackend/User/views.py
+++ notesharebackend/User/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from rest_framework import viewsets, status
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from .models import User
 from .serializers import UserSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import generics
+from django.contrib.auth.hashers import make_password
+
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
@@ -35,4 +41,21 @@ class CustomAuthToken(ObtainAuthToken):
             'email': user.email
         })           
 
+class UserRegisterView(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        data['password'] = make_password(data['password'])
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        user = serializer.instance
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        }, status=status.HTTP_201_CREATED)
 
