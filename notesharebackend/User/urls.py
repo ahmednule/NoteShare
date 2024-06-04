f7ec2686bf71df57a3d0163626222c275bef8276
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-06-04 12:06:52 +0300

    Fix some bugs

--- notesharebackend/User/urls.py
+++ notesharebackend/User/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserViewSet
+from .views import UserViewSet, CustomAuthToken
 
 urlpatterns = [
     path('get_user/<int:id>', UserViewSet.as_view({'get': 'get'})),
--- notesharebackend/User/views.py
+++ notesharebackend/User/views.py
@@ -7,6 +7,8 @@ from .serializers import UserSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
